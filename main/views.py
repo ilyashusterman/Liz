8b0c7e936290fd61f39c2aa4ab422e01a869f5e8
ilya shusterman <ilyashusterman@outlook.com>
2017-10-04 23:47:36 +0300

    added error not found page

--- main/views.py
+++ main/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-
+from django.db.models import ObjectDoesNotExist
 # Create your views here.
 from .models import Trip
 
@@ -11,6 +11,11 @@ def home(request):
     }
     return render(request, 'home.html', context=home_context)
 
+
 def detail(request, trip_id):
-    trip = Trip.objects.get(id=trip_id)
-    return render(request, 'detail.html', {'trip': trip})
+    try:
+        trip = Trip.objects.get(id=trip_id)
+        return render(request, 'detail.html', {'trip': trip})
+    except ObjectDoesNotExist as e:
+        return render(request, '404.html')
+
