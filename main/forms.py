5f13bbf4e6e309b406e8dee73953f722fc612d98
ilya shusterman <ilyashusterman@outlook.com>
2017-10-07 03:10:45 +0300

    refactored trip image url to image

--- main/forms.py
+++ main/forms.py
@@ -5,4 +5,4 @@ from .models import Trip
 class TripForm(forms.ModelForm):
     class Meta:
         model = Trip
-        fields = ['name', 'destination', 'stops', 'img_url']
+        fields = ['name', 'destination', 'stops', 'image']
--- main/models.py
+++ main/models.py
@@ -7,8 +7,8 @@ class Trip(models.Model):
     name = models.CharField(max_length=100)
     destination = models.CharField(max_length=100)
     stops = models.CharField(max_length=100)
-    img_url = models.ImageField(upload_to='trip_images',
-                                default='media/default.png')
+    image = models.ImageField(upload_to='trip_images',
+                              default='media/default.png')
 
     def __str__(self):
         return self.name
