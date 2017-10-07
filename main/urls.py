114263d48274b8e11ff3e91d7b731c93fe380877
ilya shusterman <ilyashusterman@outlook.com>
2017-10-07 03:09:08 +0300

    refactored trip model image url field to image url with default image . need to complete refactoring

--- main/urls.py
+++ main/urls.py
@@ -1,19 +1,6 @@
-"""ForLiza URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/1.11/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  url(r'^$', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  url(r'^$', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.conf.urls import url, include
-    2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
-"""
 from django.conf.urls import url
+from django.conf import settings
+from django.views.static import  serve
 from . import views
 
 urlpatterns = [
@@ -21,3 +8,9 @@ urlpatterns = [
     url(r'^([0-9]+)/$', views.detail, name='detail'),
     url(r'^add_trip/$', views.add_trip, name='add_trip'),
 ]
+
+if settings.DEBUG:
+    urlpatterns += [
+        url(r'^media/(?P<path>.*)$', serve,
+            {'document_root': settings.MEDIA_ROOT,}),
+    ]
