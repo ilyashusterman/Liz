8fd222a20f9032409dd4a7ebcb9c9f5db0614676
Ilya Shusterman <ilya@allaspect.com>
2017-10-18 15:30:15 +0300

    minor code fix

--- Liz/settings.py
+++ Liz/settings.py
@@ -17,7 +17,7 @@ PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 STATIC_URL = '/static/'
-
+STATIC_ROOT = '../main/static'
 # Extra places for collectstatic to find static files.
 STATICFILES_DIRS = (
     os.path.join(PROJECT_ROOT, '../main/static'),
